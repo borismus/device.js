8ed6644ec99e3505e621117e21f08eea9eb24c8b
Boris Smus <boris@smus.com>
2012-05-04 23:02:17 +0100

    Removed old tests.

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,3 @@
-test('Test the media query parser', function() {
-  var tags = [
-      {name: 'google-chrome', network: 'stackoverflow'},
-      {name: 'html5', network: 'stackoverflow'},
-      {name: 'html5', network: 'gamedev'}
-  ];
-  var ql = new st.QuestionList();
-  ql.setTags(tags);
-  equals(ql.tags.length, 3, 'Correct number of tags');
-  equals(ql.tags[0].name, 'google-chrome', 'Tag names do match');
-  equals(ql.tags[0].getNetwork().root, 'stackoverflow.com',
-      'Root URLs match');
+test('TODO: WRITE ME', function() {
+  // No tests yet.
 });
